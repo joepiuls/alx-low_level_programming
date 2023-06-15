31798e2282d1b78a357de8b81d800c3e56febab2
joepiuls <john71688@gmail.com>
2023-06-15 15:54:14 +0000

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
-//main is a function that prints a string
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 
-return (0); //returns zero i.e nothing if funtion is successfull
+return (0); 
 }
