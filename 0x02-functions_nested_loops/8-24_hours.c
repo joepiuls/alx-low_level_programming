9e6a22ded23b85c48ad27ea171f445760a239221
joepiuls <john71688@gmail.com>
2023-06-21 15:26:23 +0000

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,14 +9,15 @@ int i, j;
 i = 0;
 while (i < 24)
 {
-j=0;
-while(j < 60)
+j = 0;
+while (j < 60)
 {
-_putchar((i/10) + '0');
-_putchar((i%10) + '0');
+_putchar((i / 10) + '0');
+_putchar((i % 10) + '0');
 _putchar((':'));
-_putchar((j/10) + '0');
-_putchar((j%10) + '0');
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
+_putchar('\n');
 j++;
 }
 i++;
