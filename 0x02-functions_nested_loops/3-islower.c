f74f94d694f10ce45fe015fefd05a725bda2480b
joepiuls <john71688@gmail.com>
2023-06-21 13:47:48 +0000

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
 * _islower - check if alphabet is lowercase
+*@c: letter beign tested
 * return 1 if lowercase is true
 * return 0 iflowercase is false
 */
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
+	if (c >= 97 && c <= 122)
 	{
 		return ('1');
 	}
