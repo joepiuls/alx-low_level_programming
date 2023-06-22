2891af340a8008fab0ebf91909d250d2b4b39467
joepiuls <john71688@gmail.com>
2023-06-22 23:03:20 +0000

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,25 +2,28 @@
 #include <stdio.h>
 
 /**
- * print_square - Print square line dependent on the value n.
- * @n : The number of lines using '#' characters to use per row and column
+ * print_diagonal - Print diagonal line dependent on the integer n.
+ * @n : The number of lines using '\' characters to use
  * Return: Void.
  */
-void print_square(int n)
+void print_diagonal(int n)
 {
 	int i;
 	int j;
 
 	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j < n; j++)
+		for (j = 0; j < i; j++)
 		{
-			_putchar('#');
+			_putchar(' ');
 		}
+		_putchar('\$');
 		_putchar('\n');
 	}
+
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 }
+
