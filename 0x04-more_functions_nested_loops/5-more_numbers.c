a5c1f916b25e3eb8b68c4a0142ef8b5012b54030
joepiuls <john71688@gmail.com>
2023-06-22 12:34:44 +0000

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * more_numbers - Print numbers between 0 to 14.
+ *
+ * Return: Void.
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			_putchar(j + '0');
+		}
+	}
+	_putchar('\n');
+}
