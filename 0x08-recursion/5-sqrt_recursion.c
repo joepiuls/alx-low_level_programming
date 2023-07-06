42b5ea11ad594bfe4c45719aca712f7331e9d1b5
joepiuls <john71688@gmail.com>
2023-07-06 04:37:00 +0000

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - main funct
+ * @n: int n
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if ((i * i) > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (_sqrt(n, i + 1));
+}
+
