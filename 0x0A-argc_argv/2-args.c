fee216b4a7647994e228ecee078cad90fa91a77d
joepiuls <john71688@gmail.com>
2023-07-07 18:59:08 +0000

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,9 +9,10 @@
 int main(int argc, char *argv[])
 {
 	int arg;
+
 	for (arg = 0; arg < argc; arg++)
 	{
-		printf("%s\n", argv[arg]);
-		return (0);
+		printf("%s\n", argv[arg];
 	}
+	return (0);
 }
