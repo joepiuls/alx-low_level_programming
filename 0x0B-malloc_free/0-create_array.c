a88618cd19be88a53a7d0d7feb7ed9bd5e808402
joepiuls <john71688@gmail.com>
2023-07-10 11:03:55 +0000

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib>
+#include "main.h"
+/**
+ * create_array - creates an array of characters.
+ * @size: size of the array.
+ * @c: storaged char
+ *
+ * Return: pointer of an array of chars
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *cht;
+	unsigned int i;
+
+	if (size == NULL)
+		return (NULL);
+
+	cht = malloc(sizeof(c) * size);
+	
+	if (cht == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		cht[i] = c;
+	
+	return (cht);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+This is project about dynamic memory allocation using malloc function done at alx sofware engineering program.
