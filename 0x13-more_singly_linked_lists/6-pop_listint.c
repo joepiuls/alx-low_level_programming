6425ffa3e4f8e168e7484cef611c4dafa4de90f6
joepiuls <john71688@gmail.com>
2023-08-01 16:54:33 +0000

    6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+nclude "lists.h"
+#include "stdlib.h"
+/**
+ * pop_listint - pop the first element in a linked list.
+ * @head: Pointer to a list.
+ * Return: Integer(the value of the popped list)
+ **/
+
+int pop_listint(listint_t **head)
+{
+	listint_t *ptr;
+	int lists_value;
+
+	if (!(*head != NULL))
+		return (0);
+
+	ptr = *head;
+	*head = ptr->next;
+	lists_value = ptr->n;
+	free(ptr);
+	return (lists_value);
+}
