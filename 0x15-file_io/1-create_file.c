3a092cebae7b96e7938de8d61d8321b4eb81c685
joepiuls <john71688@gmail.com>
2023-08-09 00:40:46 +0000

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,50 +2,50 @@
 #include <stddef.h>
 
 /**
- * _strlen - dtermines the string length
+ * _strlen - determine string length
  * @str: string to be used
  *
  * Return: length of the string
  */
 int _strlen(char *str)
 {
-    int len = 0;
+	int len = 0;
 
-    while (str[len] != '\0')
-        len++;
-    return (len);
+	while (str[len] != '\0')
+		len++;
+	return (len);
 }
 
 /**
  * create_file - creates a file
  * @filename: name of the file
- * @text_content: content of the file which would be created
+ * @text_content: content to be created in the file
  *
  * Return: 1 on success, -1 otherwise
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fls, wrt;
+	int fl, wrt;
 
-    if (filename == NULL)
-        return (-1);
-    fls = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-    if (fls == -1)
-        return (-1);
-    if (text_content != NULL)
-    {
-        wrt = write(fls, text_content, _strlen(text_content));
-        if (wrt == -1)
-        {
-            close(fls);
-            return (-1);
-        }
-        close(fls);
-        return (1);
-    }
-    else
-    {
-        close(fls);
-        return (1);
-    }
+	if (filename == NULL)
+		return (-1);
+	fl = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fl == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		wrt = write(fl, text_content, _strlen(text_content));
+		if (wrt == -1)
+		{
+			close(fl);
+			return (-1);
+		}
+		close(fl);
+		return (1);
+	}
+	else
+	{
+		close(fl);
+		return (1);
+	}
 }
