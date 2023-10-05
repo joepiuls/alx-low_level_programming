af5018543ceb430e54f468689c7c7ee956f0968c
joepiuls <john71688@gmail.com>
2023-10-05 10:42:06 -0400

    myfirstcommit

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - functionthat implements the djb2 algorithm.
+ * @str: string used for generating hash value
+ *
+ * Return: The hash value.
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hs;
+	int i;
+
+	hs = 5381;
+	while ((i = *str++))
+		hs = ((hs << 5) + hs) + c; /* hs * 33 + c */
+
+	return (hs);
+}
